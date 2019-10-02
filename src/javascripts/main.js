7d8b7295cef0e77975ffd2d939d9706b3e73cde4
Maggie Greene <maggie.greene96@gmail.com>
2019-10-02 18:41:10 -0500

    Work in progress

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,14 @@
 import '../styles/main.scss';
 import eat from './components/eat';
 import play from './components/play';
+import sleep from './components/sleep';
 
 const init = () => {
   eat.eatActionBuilder();
   eat.hunger();
   play.playActionBuilder();
   play.gradualDecline();
+  sleep.sleepActionBuilder();
 };
 
 init();
