fa29ba34abc6fbab59860d3e6e01fb1d99284597
Maggie Greene <maggie.greene96@gmail.com>
2019-10-02 19:17:58 -0500

    Fight module complete

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,14 @@
 import '../styles/main.scss';
 import eat from './components/eat';
+import fight from './components/fight';
 import play from './components/play';
 import sleep from './components/sleep';
 
 const init = () => {
   eat.eatActionBuilder();
   eat.eatGradualDeclineSeconds();
+  fight.fightActionBuilder();
+  fight.fightGradualDeclineSeconds();
   play.playActionBuilder();
   play.playGradualDeclineSeconds();
   sleep.sleepActionBuilder();
